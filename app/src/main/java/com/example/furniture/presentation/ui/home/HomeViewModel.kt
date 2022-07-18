f2913352b0aba97e2481be524d554d476433bc10
moham <fahmyabada@gmail.com>
2022-07-18 07:48:05 +0200

    18-7-2022

---
+++ app/src/main/java/com/example/furniture/presentation/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.furniture.presentation.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/furniture/presentation/ui/register/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.furniture.presentation.ui.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
